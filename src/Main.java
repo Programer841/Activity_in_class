e3c28269bad9b62e07f6d66784545c069f32055e
CamiloP <juancamilopadillabulla@gmail.com>
2026-02-19 14:17:57 -0500

    vercion 2 experimento

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hola");
 
-        System.out.println("El archivo de Juan ha sido subido");
+        System.out.println("El archivo de Juan ha sido subido:");
+
+        System.out.println("Juan Camilo Padilla Bulla");
+        System.out.println("Hobys:");
+        System.out.println("1. Videojuegos");
+
+        System.out.println("Deportes que practico");
+        System.out.println("Correr");
         }
     }
